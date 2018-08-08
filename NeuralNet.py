bbdf9c0181c1b4a2bd3b46e524fc485692d8c1d7
Edoardo Pona <edoardo.pona@gmail.com>
2018-08-08 15:30:54 +0200

    Update NeuralNet.py

--- NeuralNet.py
+++ NeuralNet.py
@@ -76,50 +76,6 @@ def mse(y, t):
     return np.sum(1/2*(y - t)**2)
 
 
-class BatchNormalization:
-    def __init__(self, gamma):
-        # no beta as there is a bias in the network already
-        self.gamma = gamma
-        self.eps = 1e-8
-
-    def forwards(self, x):
-        self.n, self.d = x.shape
-        mean = 1/self.n * np.sum(x, axis=0)
-
-        self.xm = x - mean
-        x_sq = self.xm**2
-        self.variance = 1/self.n * np.sum(x_sq, axis=0)
-
-        self.std = np.sqrt(self.variance + 1e-8)
-        self.i_std = 1/self.std
-
-        self.x_ = self.xm * self.i_std
-
-        out = self.gamma*self.x_
-        return out
-
-    def backwards(self):
-
-        d_gamma = self.x_
-
-        dx_ = self.gamma
-        distd = dx_ * self.xm
-        dstd = distd * -1/self.variance
-        dvariance = 0.5*1/self.std*dstd
-        dsq = 1/self.n * np.ones((self.n, self.d))
-
-        dxm1 = dx_ * distd
-        dxm2 = 2*self.xm*dsq
-
-        dx1 = dxm1 + dxm2
-        dm = -np.sum((dxm1 + dxm2), axis=0)
-        dx2 = 1/self.n * np.ones((self.n, self.d))
-
-        dx = dx1 + dx2
-
-        return dx, d_gamma
-
-
 class NeuralNetwork:
 
     def __init__(self):
@@ -133,9 +89,6 @@ class NeuralNetwork:
 
         self.w2 = init_weight((128, 10))
         self.b2 = init_bias(10)
-
-        self.B2 = init_weight((10, 128))           # replaces self.w2.T if using feedback training
-        self.B1 = init_weight((128, 512))           # replaces self.w1.T
 
     def forward(self, x):
         self.x = x
@@ -181,8 +134,6 @@ class ShallowNeuralNetwork:
         self.w1 = init_weight((256, 10))
         self.b1 = init_bias(10)
 
-        self.B1 = init_weight((256, 512))
-
     def forward(self, x):
         self.x = x
 
@@ -195,12 +146,11 @@ class ShallowNeuralNetwork:
         return self.out
 
     def backwards(self, t):
-        e = (self.out - t) * d_sigmoid(self.a1)    # when using mse
+        e = (self.out - t) # * d_sigmoid(self.a1) when using mse
 
         dJdw1 = np.dot(self.h0.T, e)
         dJdb1 = np.sum(e, axis=0)
 
-        # delta0 = np.dot(e * d_sigmoid(self.a1), self.B1) * d_sigmoid(self.a0)
         delta0 = np.dot(e * d_sigmoid(self.a1), self.w1.T) * d_sigmoid(self.a0)
         dJdw0 = np.dot(self.x.T, delta0)
         dJdb0 = np.sum(delta0, axis=0)
@@ -224,7 +174,6 @@ def train_SGD(iter_num):
         out = network.forward(b)
         cost = cross_entropy(out, t)
 
-        # gradients, d_gamma = network.backwards(t)
         gradients = network.backwards(t)
 
         network.w0 -= lr * gradients[0][0]
@@ -234,8 +183,6 @@ def train_SGD(iter_num):
         network.b0 -= lr * gradients[0][1]
         network.b1 -= lr * gradients[1][1]
         # network.b2 -= lr * gradients[2][1]
-
-        # network.batch_norm.gamma -= lr * d_gamma
 
         if i % 10 == 0:
             print(i, cost, get_accuracy(out, t, batch_size))
@@ -274,39 +221,28 @@ def train_SGD_momentum(iter_num, momentum=0.9):
 
 def train_SGD_nesterov(iter_num, momentum=0.9):
     updates = [(0, 0) for i in range(n_layers)]     # [w_update, b_update] for every layer
-    # gamma_update = 0        # gamma parameter in batch norm
     for i in range(int(iter_num)):
         b, t = get_batch(batch_size)
 
         # approximating future parameters
         network.w0 -= momentum * updates[0][0]
         network.w1 -= momentum * updates[1][0]
-        # network.w2 -= momentum * updates[2][0]
 
         network.b0 -= momentum * updates[0][1]
         network.b1 -= momentum * updates[1][1]
-        # network.b2 -= momentum * updates[2][1]
-
-        # network.batch_norm.gamma -= momentum * gamma_update
 
         out = network.forward(b)
         cost = cross_entropy(out, t)
 
-        # gradients, d_gamma = network.backwards(t)
         gradients = network.backwards(t)
 
         network.w0 -= lr * gradients[0][0]
         network.w1 -= lr * gradients[1][0]
-        # network.w2 -= lr * gradients[2][0]
 
         network.b0 -= lr * gradients[0][1]
         network.b1 -= lr * gradients[1][1]
-        # network.b2 -= lr * gradients[2][1]
-
-        # network.batch_norm.gamma -= lr * d_gamma
 
         updates = [(lr*gradients[i][0] + momentum*updates[i][0], lr*gradients[i][1] + momentum*updates[i][1]) for i in range(n_layers)]
-        # gamma_update = lr*d_gamma + momentum*gamma_update
 
         if i % 10 == 0:
             print(i, cost, get_accuracy(out, t, batch_size))
@@ -314,7 +250,6 @@ def train_SGD_nesterov(iter_num, momentum=0.9):
 
 
 def train_AdaGrad(iter_num):
-    """ only works without batch normalization for now (shallow network) """
     r = [[0, 0] for i in range(n_layers)]
 
     lr_matrix_w0 = lr * np.ones(network.w0.shape)
@@ -347,7 +282,6 @@ def train_AdaGrad(iter_num):
 
 
 def train_RMSProp(iter_num, decay_rate=0.5):
-    """ only works without batch normalization for now (shallow network) """
     r = [[0, 0] for i in range(n_layers)]
 
     for i in range(int(iter_num)):
@@ -373,7 +307,6 @@ def train_RMSProp(iter_num, decay_rate=0.5):
 
 
 def train_RMSProp_nesterov(iter_num, momentum=0.9, decay_rate=0.5):
-    """ only works without batch normalization for now (shallow network) """
     updates = [[0, 0] for i in range(n_layers)]
     r = [[0, 0] for i in range(n_layers)]
 
